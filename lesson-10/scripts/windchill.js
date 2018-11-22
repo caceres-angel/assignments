1b67e0c2944eed42a1b96978cad4bbd63693bfa6
Angel Caceres <cac18001@byui.edu>
2018-11-22 08:48:45 -0300

    API test

--- lesson-10/scripts/windchill.js
+++ lesson-10/scripts/windchill.js
@@ -1,9 +1,9 @@
 
 var f = 0;
-var t = parseFloat(document.getElementById("temperature").innerText);
-var s = parseFloat(document.getElementById("windspeed").innerText);
+var t = parseFloat(document.getElementById("prestonTemp").innerText);
+var s = parseFloat(document.getElementById("prestonWS").innerText);
 s =  Math.pow(s, 0.16);
 
 f = Math.round((35.74 + .6215 * t - 35.75 * s + 0.42745 * t * s) * 100)/100;
 
-document.getElementById("windchill").innerHTML = f+"ºF";
+document.getElementById("prestonWC").innerHTML = f+"ºF";
