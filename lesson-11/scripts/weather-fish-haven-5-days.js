d769c0b3097bdf99519dd15d14c407fa7bf806c5
Angel Caceres <cac18001@byui.edu>
2018-11-29 14:49:18 -0300

    last week

---
+++ lesson-11/scripts/weather-fish-haven-5-days.js
@@ -0,0 +1,24 @@
+   
+
+    var fiveDayUrl = 'https://api.openweathermap.org/data/2.5/forecast?id=5585010&appid=20237cefe6297c195f37cf7adef74932&units=metric';
+    
+    var fiveDayRequest = new XMLHttpRequest();
+
+    fiveDayRequest.open('GET', fiveDayUrl, true);
+
+    fiveDayRequest.send();
+
+    fiveDayRequest.onload =  function () {
+
+        var fiveDayData = JSON.parse(fiveDayRequest.responseText);
+
+        console.log(fiveDayData);
+
+        document.getElementById("day1").innerHTML = fiveDayData.list[0].main.temp;
+        document.getElementById("day2").innerHTML = fiveDayData.list[7].main.temp;
+        document.getElementById("day3").innerHTML = fiveDayData.list[14].main.temp;
+        document.getElementById("day4").innerHTML = fiveDayData.list[21].main.temp;
+        document.getElementById("day5").innerHTML = fiveDayData.list[28].main.temp;
+
+    }
+
---
+++ lesson-11/scripts/weather-soda-springs.js
@@ -0,0 +1,25 @@
+   
+
+    var apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5607916&appid=20237cefe6297c195f37cf7adef74932&units=metric';
+
+    var weatherRequest = new XMLHttpRequest();
+
+    weatherRequest.open('GET', apiURL, true);
+
+    weatherRequest.send();
+
+    weatherRequest.onload =  function () {
+
+        var weatherData = JSON.parse(weatherRequest.responseText);
+
+        console.log(weatherData);
+
+        document.getElementById("prestonMain").innerHTML = weatherData.weather[0].description;
+        document.getElementById("prestonTemp").innerHTML = weatherData.main.temp;
+        document.getElementById("prestonHum").innerHTML = weatherData.main.humidity;
+        document.getElementById("prestonWS").innerHTML = weatherData.wind.speed;
+        document.getElementById("prestonWD").innerHTML = weatherData.wind.deg;
+        document.getElementById("prestonWC").innerHTML = weatherData.main.temp;
+
+    }
+
